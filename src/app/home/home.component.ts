d32d9a4288c86ceee18a2ad828c26aed8f601cc9
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2025-03-29 14:53:20 +0100

    style(animations): ajout des animations d'apparition des objets

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Spot } from '../models/spot.model';
 import { SpotService } from '../services/spot.service';
 import { inject } from '@angular/core';
@@ -23,6 +23,27 @@ export class HomeComponent {
     // Initialiser la liste des spots
     this.spotService.getAllSpots().subscribe((data: Spot[]) => {
       this.spots = data;
+    });
+
+    // Initialiser le scroll
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.checkScroll();
+  }
+
+  checkScroll() {
+    const spotCards = document.querySelectorAll('.spot-card');
+    
+    spotCards.forEach((card) => {
+      const cardPosition = card.getBoundingClientRect().top;
+      const screenPosition = window.innerHeight / 1.3;
+
+      if (cardPosition < screenPosition) {
+        card.classList.add('visible');
+      }
     });
   }
 }
